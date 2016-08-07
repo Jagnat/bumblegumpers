3ee236b27041122e87a37bd4f21b3e91b9712018
Jagannath Natarajan <jagnat4000@gmail.com>
2016-08-07 13:06:33 -0700

    Initial commit

---
+++ questionmark/Game1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace questionmark
+{
+	public class Game1 : Game
+	{
+		Render renderer;
+
+		GraphicsDeviceManager graphics;
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			graphics.PreferredBackBufferWidth = 640;
+			graphics.PreferredBackBufferHeight = 480;
+			Content.RootDirectory = "Content";
+		}
+
+		protected override void Initialize()
+		{
+			renderer = new Render(this);
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			renderer.loadGraphics(Content);
+		}
+
+		protected override void UnloadContent()
+		{
+			
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+
+			renderer.renderGame();
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ questionmark/Render.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace questionmark
+{
+	public class Render
+	{
+		Texture2D tileSheet;
+
+		SpriteBatch spriteBatch;
+		public Render(Game1 game)
+		{
+			spriteBatch = new SpriteBatch(game.GraphicsDevice);
+		}
+
+		public void loadGraphics(ContentManager content)
+		{
+			tileSheet = content.Load<Texture2D>("visual/tilesheet");
+		}
+
+		public void renderGame()
+		{
+			spriteBatch.Begin();
+
+			spriteBatch.Draw(tileSheet, new Rectangle(0, 0, 640, 480), Color.White);
+
+			spriteBatch.End();
+		}
+	}
+}
