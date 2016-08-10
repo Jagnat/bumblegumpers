3d3eb902942893a81dc32f73294ced92ae2c1736
Jagannath Natarajan <jagnat4000@gmail.com>
2016-08-09 17:19:19 -0700

    Preliminary movement isn't brokeded

--- questionmark/Player.cs
+++ questionmark/Player.cs
@@ -10,6 +10,9 @@ namespace questionmark
 	{
 		public int X, Y;
 
+		bool onGround;
+		bool canMove;
+
 		public Player(int x, int y)
 		{
 			X = x;
@@ -18,9 +21,46 @@ namespace questionmark
 
 		public void update(InputState input, World world)
 		{
-			if (input.jumpPressed && !world.collides(X, Y - 1))
+			if (world.collides(X, Y + 1))
+				onGround = true;
+			else
+				onGround = false;
+			if (onGround)
 			{
-				Y--;
+				// Left or right or jump
+				canMove = true;
+				if (input.jumpPressed && !world.collides(X, Y - 1))
+					Y--;
+				else if (input.leftPressed && !input.rightPressed && !world.collides(X - 1, Y))
+					X--;
+				else if (input.rightPressed && !input.leftPressed && !world.collides(X + 1, Y))
+					X++;
+			}
+			else if (canMove)
+			{
+				// Left or right movement
+				if (input.jumpPressed)
+				{
+					canMove = false;
+				}
+				else if (input.leftPressed && !input.rightPressed && !world.collides(X - 1, Y))
+				{
+					X--;
+					canMove = false;
+				}
+				else if (input.rightPressed && !input.leftPressed && !world.collides(X + 1, Y))
+				{
+					X++;
+					canMove = false;
+				}
+			}
+			else
+			{
+				// Move player to ground
+				int index = 0;
+				while (!world.collides(X, Y + index))
+					index++;
+				Y += index - 1;
 			}
 		}
 	}
